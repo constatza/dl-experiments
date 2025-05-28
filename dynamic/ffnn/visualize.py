ec92f61c598f61c6be929d2ad9d70e419791be72
constatza <29166969+constatza@users.noreply.github.com>
2025-05-28 16:10:40 +0300

    fix: working prediction & new config for ffnn
    - cae config needs update

--- dynamic/ffnn/visualize.py
+++ dynamic/ffnn/visualize.py
@@ -1,81 +1,27 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import torch
 
-from dlkit.postprocessing import plot_pred_vs_true, plot_residuals
-from dlkit.metrics.temporal import mase
 
 from dlkit.io.settings import load_validated_settings
 
+
 num_plots = 3
 dof_idx = 99
-variable = "U"
+variable = "latent"
 config_path = "config.toml"
 config = load_validated_settings(config_path)
 paths = config.PATHS
-output_dir = paths.figures
-output_dir.mkdir(exist_ok=True, parents=True)
 
+features = np.load(paths.features)
+targets = np.load(paths.targets)
+predictions = np.load(paths.predictions)
 
-features = np.load(paths.features, mmap_mode="r")
-plt.plot(features[:, 299, :].T)
-predictions_all = np.load(paths.predictions, mmap_mode="r")
-predictions = predictions_all["predictions"]
-latent = predictions_all["latent"]
-# stack first two axes
-predictions = predictions.reshape(-1, predictions.shape[-2], predictions.shape[-1])
-timesteps = np.arange(0, predictions.shape[-1])
-
-
-# random num_plots indices from axis 0
-sample_idx = np.random.randint(0, features.shape[0], num_plots)
-# random num_plots indices from axis 1
-
-
-selected_features = features[sample_idx, dof_idx : dof_idx + 1, :]
-selected_predictions = predictions[sample_idx, dof_idx : dof_idx + 1, :]
-
-error = mase(
-    torch.from_numpy(selected_predictions).float(),
-    torch.from_numpy(selected_features).float(),
-)
-title = f"Variable: {variable}, MASE: {error:.4f}"
-# common x-axis
-fig, ax = plt.subplots(num_plots, 1, figsize=(10, 10), sharex=True)
-fig.suptitle(title)
-for i, idx in enumerate(sample_idx):
-    ax[i].plot(timesteps, selected_features[:, 0, :].T, label="Original")
-    ax[i].plot(timesteps, selected_predictions[:, 0, :].T, label="Predicted")
-    ax[i].set_title(f"Sample {idx}")
-    ax[i].legend()
-    ax[i].set_xlabel("Timestep")
-    ax[i].set_ylabel(f"{variable}")
-# common x-axis
-fig.savefig(output_dir / f"{variable}_predictions.png", dpi=600)
-
-
-fig = plot_pred_vs_true(selected_predictions.ravel(), selected_features.ravel(), title)
-fig.savefig(output_dir / f"{variable}_pred_vs_true.png", dpi=600)
-
-
-fig = plot_residuals(selected_predictions.ravel(), selected_features.ravel(), title)
-fig.savefig(output_dir / f"{variable}_residuals.png", dpi=600)
+plt.scatter(features[:, 0], targets[:, 0],  label="Targets")
+plt.scatter(features[:, 1], predictions[:, 0], label="Predicted")
+plt.legend()
 plt.show()
 
-# visualize latent space
-# add figure with three 3d subplots
-fig = plt.figure(figsize=(15, 10))
-for i in range(3):
-    ax = fig.add_subplot(1, 3, i + 1, projection="3d")
-    scatter = ax.scatter(latent[:, 0], latent[:, 1], latent[:, 2])
-    ax.set_xlabel("Latent 1")
-    ax.set_ylabel("Latent 2")
-    ax.set_zlabel("Latent 3")
-    # add colorbar
-    cbar = plt.colorbar(scatter, ax=ax, location="bottom")
-    cbar.set_label(f"Problem Parameter {i + 1}")
-fig.suptitle("Latent Space")
-fig.savefig(output_dir / f"{variable}_latent_space.png", dpi=600)
 
-plt.show()
+
+
--- dynamic/predict.py
+++ dynamic/predict.py
@@ -1,11 +1,11 @@
 import json
-from pathlib import Path
+import sys
 
-import click
 import numpy as np  # noqa: D100
 import torch
 
-from dlkit.networks.blocks.basic_network import PipelineNetwork
+from dlkit.networks.blocks.base import PipelineNetwork
+from loguru import logger
 
 
 def main(features_path, solution_path, decoder_path, ffnn_path):
@@ -25,15 +25,19 @@ def main(features_path, solution_path, decoder_path, ffnn_path):
         x = ffnn(x)
         y = cae.model.decoder(x).cpu().numpy()
 
-    np.save(solution_path, y)
+    np.save(solution_path, np.squeeze(y))
+    logger.info(f"Prediction saved to {solution_path}")
 
 
-click.command()
-click.argument("settings-path", type=str, help="Path to the settings file")
-
-
+# click.command("main")
+# click.argument("settings_path", type=str, help="Path to the settings file")
+# click.argument("results_file", type=str, help="Path to the timings file")
+# click.argument("log_file", type=str, help="Path to the error log file")
 def main_cli(
-    settings_path: str = r"M:\shared\Serafeim_Atzarakis\results\CantileverDynamicLinear\settings_sample.json",
+    # settings_path: str = r"M:\shared\Serafeim_Atzarakis\results\CantileverDynamicLinear\settings_sample.json",
+    settings_path,
+    results_file,
+    log_file,
 ):
     """Main function to run the prediction script.
 
@@ -53,10 +57,14 @@ def main_cli(
 # region debug
 if __name__ == "__main__":
     # Actual script
-    parent = Path(r"M:\shared\Serafeim_Atzarakis\results\CantileverDynamicLinear")
-    main(
-        features_path=parent / "test_model_params.npy",
-        solution_path=parent / "test_solutions.npy",
-        decoder_path=parent / r"checkpoints\cae.ckpt",
-        ffnn_path=parent / r"checkpoints\ffnn.ckpt",
-    )
+    # parent = Path(r"M:\shared\Serafeim_Atzarakis\results\CantileverDynamicLinear")
+    # main(
+    #     features_path=parent / "test_model_params.npy",
+    #     solution_path=parent / "test_solutions.npy",
+    #     decoder_path=parent / r"checkpoints\cae.ckpt",
+    #     ffnn_path=parent / r"checkpoints\ffnn.ckpt",
+    # )
+    path_settings = sys.argv[1]
+    results_file = sys.argv[2]
+    log_file = sys.argv[3]
+    main_cli(path_settings, results_file, log_file)
